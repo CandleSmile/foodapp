510a09f1a38e3ead3831303fc0ff3c8cd12a8e1e
Elizaveta <lizaveta.razumovich@gmail.com>
2023-02-04 16:23:43 +0300

    base url api

--- src/api/configapi.js
+++ src/api/configapi.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 //new api
-//const baseUrl = "https://foodapi.somee.com/api";
-const baseUrl = "https://localhost:7266/api";
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://foodapi.somee.com/api"
+    : "https://localhost:7266/api";
 let axiosNew = axios.create();
 axiosNew.defaults.withCredentials = true;
 axiosNew.defaults.baseURL = baseUrl;
